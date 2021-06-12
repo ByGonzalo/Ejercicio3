c1c6a45e8ef0e5cd7147112030d5e3c1f073d28e
ByGonzalo <84694563+ByGonzalo@users.noreply.github.com>
2021-06-12 15:49:25 -0500

    Add files via upload

---
+++ Program5.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp8
+{
+    class Program
+    {       
+
+        static void Main(string[] args)
+        {
+            Thread H1 = new Thread(new ThreadStart(pro1));            
+            Thread H2 = new Thread(new ThreadStart(pro2));            
+            Thread H3 = new Thread(new ThreadStart(pro3));
+            H1.Start();
+            H2.Start();
+            H3.Start();
+        }
+        public void pro1()
+        {
+            int i ;
+            int p=0;
+            for(i = 0; i <= 10; i++)
+            {
+                p = i * 5;
+                Console.WriteLine(i + " * 5 = " + p);
+            }            
+        }
+        public void pro2()
+        {
+            int i ;
+            int p = 0;
+            for (i = 0; i <= 10; i++)
+            {
+                p = i * 10;
+                Console.WriteLine(i + " * 10 = " + p);
+            }            
+        }
+        public void pro3()
+        {
+            int i ;
+            int p = 0;
+            for (i = 0; i <= 15; i++)
+            {
+                p = i * 5;
+                Console.WriteLine(i + " * 15 = " + p);
+            }            
+        }
+    }
+}
